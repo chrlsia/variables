a841549ef3eb1dff3b6142c0899c4fe4f2f60ef0
Chris Siannas <xrsia@otenet.gr>
2022-01-01 09:53:56 +0200

    Start

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	//one way, declareand then assign (2 steps)
+	var firstNumber int
+	firstNumber = 2
+
+	//second way
+	var secondNumber = 5
+
+	//3rd way
+	subtraction:=7
+
+}
